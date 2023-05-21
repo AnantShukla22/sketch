61aeebcd2d717272044f0f736d078a5326728bcc
anantshukla22 <anant.official2201@gmail.com>
2023-05-21 15:30:03 +0530

    responsive

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
         <Statistics />
         <Accordian />
         <Contact />
-        {/* <Footer /> */}
+        <Footer />
       </section>
     </>
   );
